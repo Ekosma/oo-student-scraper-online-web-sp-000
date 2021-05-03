c67fba8649aa1ab7396e24c1ff875c174020d0b6
Evelyn Kosma <evelyn.a.kosma@gmail.com>
2021-05-03 01:36:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,6 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each{|student| Student.new(student)}
-    end
   end
 
   def add_student_attributes(attributes_hash)
